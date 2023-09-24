295493c930c04fc29983a25e3078a6edd13f59e8
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-09-24 10:49:59 +0700

    Update students at Mindx JS

---
+++ js/students.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function () {
+  var target = document.querySelector('#customer_style');
+  var targetPos = target.getBoundingClientRect().top + window.pageYOffset;
+
+  var scrollPos = window.scrollY + window.innerHeight;
+  if (scrollPos >= targetPos) {
+    document.querySelector('.customer_loading').classList.add('show');
+  }
+});
