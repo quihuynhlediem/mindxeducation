a3adf739b611a35b272badcb4d0ce927ca9634b0
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-10-10 19:19:56 +0700

    Update change tab function - store tab choice history

--- js/blog.js
+++ js/blog.js
@@ -2,22 +2,27 @@
 let tabChangeItems = document.querySelectorAll(".tab-change-inner");
 let textChangeItems = document.querySelectorAll("span.text-change-inner");
 
-function tabChange() {
-	let activeTab = this.event.target.parentElement;
-	let activeText = this.event.target;
-	let activeId = activeText.id;
-	activeTab.className = "tab-change-inner tab-change-items-active";
+// Check whether tab id is stored in local storage
+if (localStorage.getItem("activeTabId") === null) {
+  localStorage.setItem("activeTabId", "1");
+};
+
+function tabChangeProcess() {
+  let activeTab = document.getElementById(localStorage.getItem("activeTabId")).parentElement;
+  let activeText = document.getElementById(localStorage.getItem("activeTabId"));
+  let activeId = activeText.id;
+  activeTab.className = "tab-change-inner tab-change-items-active";
   activeText.className = "text-change-inner tab-change-text-active";
   
   for (let i = 0; i < tabChangeItems.length; i++) {
-		if (activeId != textChangeItems[i].id) {
-			tabChangeItems[i].className = "tab-change-inner tab-change-items";
-			textChangeItems[i].className = "text-change-inner tab-change-text";
-		}
-	}
+    if (activeId != textChangeItems[i].id) {
+      tabChangeItems[i].className = "tab-change-inner tab-change-items";
+      textChangeItems[i].className = "text-change-inner tab-change-text";
+    }
+  }
 
-	// Filter blog posts by clicking tab
-	let blogItems = document.querySelectorAll(".blog-items");
+  // Filter blog posts by clicking tab
+  let blogItems = document.querySelectorAll(".blog-items");
   if (activeText.innerText === "Tất cả") {
     for (let i = 0; i < blogItems.length; i++) {
       blogItems[i].style.display = "block";
@@ -33,4 +38,12 @@ function tabChange() {
       }
     }
   }
+}
+
+tabChangeProcess();
+
+function tabChange() { 
+  let activeTabId = this.event.target.id;
+  localStorage.setItem("activeTabId", activeTabId);
+  tabChangeProcess();
 }
