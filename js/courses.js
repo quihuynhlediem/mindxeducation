09a00e9fcf4eac09968dd66a28d82af370810892
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-10-10 19:29:08 +0700

    Add courses filter by clicking tab

--- js/courses.js
+++ js/courses.js
@@ -1,3 +1,4 @@
+// Add course function
 let coursesArray = localStorage.getItem("courses") ? JSON.parse(localStorage.getItem("courses")) : [];
 
 function addCourse() {
@@ -21,4 +22,54 @@ for (let i = 0; i < mindxCourseArray.length; i++) {
     enrolledCourseBtn.innerHTML = "Đã đăng kí";
     enrolledCourseBtn.style.backgroundColor = "#982a26";
   };
+}
+
+// Course filter by clicking tab
+let tabChangeItems = document.querySelectorAll(".tab-change-inner");
+let textChangeItems = document.querySelectorAll("span.text-change-inner");
+
+// Check whether tab id is stored in local storage
+if (localStorage.getItem("activeCourseId") === null) {
+  localStorage.setItem("activeCourseId", "1");
+};
+
+function tabChangeProcess() {
+  let activeTab = document.getElementById(localStorage.getItem("activeCourseId")).parentElement;
+  let activeText = document.getElementById(localStorage.getItem("activeCourseId"));
+  let activeId = activeText.id;
+  activeTab.className = "tab-change-inner tab-change-items-active";
+  activeText.className = "text-change-inner tab-change-text-active";
+  
+  for (let i = 0; i < tabChangeItems.length; i++) {
+    if (activeId != textChangeItems[i].id) {
+      tabChangeItems[i].className = "tab-change-inner tab-change-items";
+      textChangeItems[i].className = "text-change-inner tab-change-text";
+    }
+  }
+
+  // Filter courses by clicking tab
+  let courseItems = document.querySelectorAll(".course-items");
+  if (activeText.innerText === "Tất cả") {
+    for (let i = 0; i < courseItems.length; i++) {
+      courseItems[i].style.display = "block";
+    }
+  } else {
+    for (let i = 0; i < courseItems.length; i++) {
+      let currentCourseLabel =
+        courseItems[i].firstElementChild.nextElementSibling.innerText;
+      if (activeText.innerText === currentCourseLabel) {
+        courseItems[i].style.display = "block";
+      } else {
+        courseItems[i].style.display = "none";
+      }
+    }
+  }
+}
+
+tabChangeProcess();
+
+function tabChange() { 
+  let activeCourseId = this.event.target.id;
+  localStorage.setItem("activeCourseId", activeCourseId);
+  tabChangeProcess();
 }
